f53fabd39d8492e88d56c24a46e62e97ce57e909
rasitechnology <rasitechnology@gmail.com>
2022-11-25 10:40:37 +0700

    masih error di home fragment

--- app/src/main/java/com/ssrdi/co/id/myradboox/fragmentreseller/HomeFragment.kt
+++ app/src/main/java/com/ssrdi/co/id/myradboox/fragmentreseller/HomeFragment.kt
@@ -10,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView.LayoutManager
@@ -78,7 +79,7 @@ class HomeFragment : Fragment() {
 //        mLayoutManager = LinearLayoutManager(requireContext())
 //        binding.rvM.layoutManager = mLayoutManager
 //        binding.rvM.setHasFixedSize(true)
-    }
+
 
     private fun setRVScrollListener() {
         mLayoutManager = LinearLayoutManager(requireContext())
