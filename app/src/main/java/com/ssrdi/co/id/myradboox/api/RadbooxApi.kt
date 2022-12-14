f6bc9c9d315243433ed73883835c796d107500c3
pratamawijaya <set.mnemonix@gmail.com>
2022-12-14 11:07:25 +0700

    update api

--- app/src/main/java/com/ssrdi/co/id/myradboox/api/RadbooxApi.kt
+++ app/src/main/java/com/ssrdi/co/id/myradboox/api/RadbooxApi.kt
@@ -1,5 +1,6 @@
 package com.ssrdi.co.id.myradboox.api
 
+import com.ssrdi.co.id.myradboox.api.model.ResellerResponse
 import com.ssrdi.co.id.myradboox.model.*
 import retrofit2.Call
 import retrofit2.http.*
@@ -56,17 +57,8 @@ interface RadbooxApi {
         @Field("total_profit_reseller") total_profit_reseller: Int,
         @Field("balance") balance: Int,
         @Field("amount_due") amount_due: Int,
-    @POST("")
-    fun generateVoucher(
-        @Field("stok_voucher") stok_voucher: Int,
-        @Field("active_voucher") active_voucher: Int,
-        @Field("total_sales") total_sales: Int,
-        @Field("total_amount") total_amount: Int,
-        @Field("total_deposit") total_deposit: Int,
-        @Field("total_profit_reseller") total_profit_reseller: Int,
-        @Field("balance") balance: Int,
-        @Field("amount_due") amount_due: Int,
     ): Call<GenerateResponse>
+
 
     @GET("reseller/options")
     fun getResellerOptions(
